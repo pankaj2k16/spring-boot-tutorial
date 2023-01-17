e4cbeb6f6e19f3d919b6600f4eb65c1946755ad7
Pankaj Kumar <pankaj.kumar@wissen.com>
2023-01-17 08:35:22 +0530

    Initial commit

---
+++ src/test/java/com/epankaj/Springboot/tutorial/SpringBootTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epankaj.Springboot.tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
